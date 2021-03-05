bf9ad796d604f2db7790a4cd452c6ada648e34c2
fabioreszende <fabiorezendefilho@gmail.com>
2021-03-05 18:25:17 -0300

    up
    up

--- app/src/main/java/com/example/studentcalculator/MainActivity.kt
+++ app/src/main/java/com/example/studentcalculator/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.studentcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btCalculation = calculate
+        val resultado = result
+
+        btCalculation.setOnClickListener {
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val media = (nota1 + nota2)/2
+            val faltas = Integer.parseInt(faltas.text.toString())
+
+            if(media >= 6 && faltas <= 10){
+                resultado.setText("Aluno foi Aprovado \n Nota Final: $media \n Faltas : $faltas")
+            }
+
+        }
     }
 }
